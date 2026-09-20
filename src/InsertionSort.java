10a96a3d49532aad62ff536e59bdd58a3c505031
Zicker4U <bunheng.lbh@gmail.com>
2026-09-20 22:29:17 +0700

    Implement selection sort and insertion sort

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,9 +1,38 @@
 /** P3: Insertion Sort. Shifts are movements: call m.move() for each shift, not m.swap(). */
 public class InsertionSort implements Sorter {
-    public String name() { return "Insertion Sort"; }
-    public boolean isQuadratic() { return true; }
+
+    public String name() {
+        return "Insertion Sort";
+    }
+
+    public boolean isQuadratic() {
+        return true;
+    }
 
     public void sort(int[] arr, Metrics m) {
-        // TODO (P3)
+
+        // Start from index 1 because index 0 is already "sorted"
+        for (int i = 1; i < arr.length; i++) {
+
+            // Save the value we want to insert
+            int key = arr[i];
+
+            // Start checking from the element before key
+            int j = i - 1;
+
+            // Shift larger elements one position to the right
+            while (j >= 0 && m.greater(arr[j], key)) {
+
+                arr[j + 1] = arr[j];
+
+                // Count this shift as a movement
+                m.move();
+
+                j--;
+            }
+
+            // Insert key into its correct position
+            arr[j + 1] = key;
+        }
     }
 }
--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,9 +1,35 @@
 /** P3: Selection Sort. Use m.less(...) for comparisons and m.swap(...) for swaps. */
 public class SelectionSort implements Sorter {
-    public String name() { return "Selection Sort"; }
-    public boolean isQuadratic() { return true; }
+
+    public String name() {
+        return "Selection Sort";
+    }
+
+    public boolean isQuadratic() {
+        return true;
+    }
 
     public void sort(int[] arr, Metrics m) {
-        // TODO (P3)
+
+        // Move through the array one position at a time
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            // Assume the current position contains the smallest value
+            int minIndex = i;
+
+            // Search the remaining unsorted part of the array
+            for (int j = i + 1; j < arr.length; j++) {
+
+                // Compare arr[j] with the current smallest value
+                if (m.less(arr[j], arr[minIndex])) {
+                    minIndex = j;
+                }
+            }
+
+            // Put the smallest value into position i
+            if (minIndex != i) {
+                m.swap(arr, i, minIndex);
+            }
+        }
     }
 }
